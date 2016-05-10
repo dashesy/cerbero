b6a027c1e4db0a58b00527faf03c641d1471eb28
Nirbheek Chauhan <nirbheek@centricular.com>
2016-05-10 19:57:57 +0530

    cerbero: Implement selecting of build type via a command-line argument
    This also reports the currently-configured build type if ~/.cerbero/cerbero.cbc
    is used to set the debug variant.

--- cerbero/commands/package.py
+++ cerbero/commands/package.py
@@ -19,7 +19,7 @@
 import os
 
 from cerbero.config import Platform
-from cerbero.commands import Command, register_command, build
+from cerbero.commands import Command, register_command, build, set_buildtype
 from cerbero.utils import _, N_, ArgparseArgument
 from cerbero.utils import messages as m
 from cerbero.errors import PackageNotFoundError, UsageError
@@ -39,6 +39,9 @@ class Package(Command):
                              help=_('name of the package to create')),
             ArgparseArgument('-o', '--output-dir', default='.',
                              help=_('Output directory for the tarball file')),
+            ArgparseArgument('--buildtype', default=None,
+                             help=_('specifies the build type: "debug", "release" '
+                             'or "debugoptimized" (default)')),
             ArgparseArgument('-t', '--tarball', action='store_true',
                 default=False,
                 help=_('Creates a tarball instead of a native package')),
@@ -65,6 +68,7 @@ class Package(Command):
             ])
 
     def run(self, config, args):
+        set_buildtype(config, args.buildtype)
         self.store = PackagesStore(config)
         p = self.store.get_package(args.package[0])
 
